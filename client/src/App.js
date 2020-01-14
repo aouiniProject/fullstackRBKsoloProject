71601f89a9e24b840187f31d3de6e730472b916f
aminaouini <aminaouini565@gmail.com>
2020-01-14 08:31:37 +0100

    functional sign up with functionalities back/front

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import SignUp from './components/auth/signup.js'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <SignUp></SignUp> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ database/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const url = `mongodb://localhost:27017/amin`;
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    name: { type: String, required: true },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+    password: { type: String, required: true }
+})
+
+const User = mongoose.model('Users', UserSchema);
+
+const insertUser = (data, cb) => {
+
+    // bcrypt.genSalt(10, function (err, salt) {
+    //     bcrypt.hash(data.password, salt, function (err, hash) {
+    //         data.password = hash;
+    //     });
+    var user = new User(data)
+
+    user.save()
+        .catch(err => console.log('this is a nono !!! : ' + err))
+
+}
+
+
+
+const findUser = (name, email, cb) => {
+    User.findOne({ name: name, email: email }, cb);
+};
+
+
+
+
+
+exports.User = User;
+exports.insertUser = insertUser;
+exports.findUser = findUser;
